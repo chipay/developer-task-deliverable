e028545fe245803b9e76ff217150d81d1db03a66
chipay <germans@gmail.com>
2014-01-06 01:26:19 -0300

    added a simple schema validation for the input file

--- src/GSibay/DeveloperTask/Command/SortDatesDescendingExcludingPrimeYearsCommand.php
+++ src/GSibay/DeveloperTask/Command/SortDatesDescendingExcludingPrimeYearsCommand.php
@@ -3,12 +3,12 @@
 namespace GSibay\DeveloperTask\Command;
 
 use Symfony\Component\Console\Input\InputArgument;
-
 use GSibay\DeveloperTask\Service\ArrayOrganizerService;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Command\Command;
 use JMS\Serializer\SerializerInterface;
+use \DOMDocument as DOMDocument;
 
 /**
  * Command to parse a file into an array and
@@ -44,20 +44,39 @@ class SortDatesDescendingExcludingPrimeYearsCommand extends Command
     private $serializeFormat;
 
     /**
-     * Constructor.
-     *
-     * @param GSibay\DeveloperTask\Arrays\ArrayOrganizerService $dateGeneratorService
-     * @param JMS\Serializer\SerializerInterface                $serializer
-     * @param string                                            $serializeFormat      [optional] Format of the serialization.
+     * The file containing the schema to validate the xml input
+     * @var string
      */
-    public function __construct(ArrayOrganizerService $arrayOrganizerService, SerializerInterface $serializer, $serializeFormat = 'xml')
+    private $schemaForValidationFileName;
+    
+    /**
+     * 
+     * @param ArrayOrganizerService $arrayOrganizerService
+     * @param SerializerInterface $serializer
+     * @param string $serializeFormat
+     * @param string $schemaForValidationFileName
+     * @throws \RuntimeException
+     */
+    public function __construct(ArrayOrganizerService $arrayOrganizerService, SerializerInterface $serializer, $schemaForValidationFileName = null, $serializeFormat = 'xml')
     {
         parent::__construct();
         $this->serializer = $serializer;
         $this->arrayOrganizerService = $arrayOrganizerService;
         $this->serializeFormat = 'xml';
+        $this->schemaForValidationFileName = $schemaForValidationFileName;
     }
 
+    private function validateXMLFile($xmlStr)
+    {
+        if($this->schemaForValidationFileName !== null) {
+            $xml= new DOMDocument();
+            $xml->loadXML($xmlStr, LIBXML_NOBLANKS);
+            if(!$xml->schemaValidate($this->schemaForValidationFileName)) {
+                throw new \RuntimeException("Input file is not valid according to schema " . $this->schemaForValidationFileName); 
+            }
+        }
+    }
+    
     /**
      * (non-PHPdoc)
      * @see Symfony\Component\Console\Command.Command::configure()
@@ -65,14 +84,13 @@ class SortDatesDescendingExcludingPrimeYearsCommand extends Command
     protected function configure()
     {
         $this->setName(self::COMMAND_NAME)
-        ->setDescription('Parses the dates from the input file and sorts them excluding years that are prime numbers. The result is
-                saved to the output file')
-        ->addArgument(self::INPUT_FILE_NAME_ARG, InputArgument::REQUIRED, "Name of the input file")
-        ->addArgument(self::OUTPUT_FILE_NAME_ARG, InputArgument::REQUIRED, "Name of the output file")
+        ->setDescription('Parses the dates from the input xml file and sorts them excluding years that are prime numbers. The result is transformed
+                to PST timezone and set to 13:00:00. The result is saved to the output file.')
+        ->addArgument(self::INPUT_FILE_NAME_ARG, InputArgument::REQUIRED, "Name of the input file.")
+        ->addArgument(self::OUTPUT_FILE_NAME_ARG, InputArgument::REQUIRED, "Name of the output file.")
         ->setHelp("The <info>sort-dates-epy</info> command parses the input file and creates an output file with
-                the dates sorted excluding years that are prime numbers. Input file should have dates generated at 13:00:00 GMT.
-                The output file will have those dates but transformed by changing the timezone to PST, setting the time to 13:00:00 PST and
-                finally generating the date");
+                the dates sorted excluding years that are prime numbers. The output file will have those dates but transformed by changing the timezone to PST, 
+                setting the time to 13:00:00 PST.");
     }
 
     /**
@@ -87,7 +105,9 @@ class SortDatesDescendingExcludingPrimeYearsCommand extends Command
             throw new \RuntimeException('Could not read input file: '.$input->getArgument(self::INPUT_FILE_NAME_ARG));
         }
 
-        //TODO: check that the data is in the right format? is an xml at least? a schema?
+        // validate the xml
+        $this->validateXMLFile($fileContent); 
+        
         $dates = $this->serializer->deserialize($fileContent, 'array', $this->serializeFormat);
 
         // sort and filter the dates
